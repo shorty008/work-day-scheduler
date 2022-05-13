1a6c012423131a6bf5065fb1a462f22ff05c51eb
Robbie K <rkahl08@gmail.com>
2022-05-13 13:49:52 -0500

    update save function and index file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,23 @@ var todayDate = function (){
 }
 
 todayDate();
+
+//Save and load tasks on local storage
+var saveTask = function (){
+    $(".saveBtn").on("click", function() {
+        //get value of content from sibling div
+        var message = $(this).siblings(".description").val();
+        //get value of time from parent div
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, message);
+    })
+
+    //loop to display stored values
+    for (var i = 5; i <18; i++ ) {
+        var number = i;
+        $("#" + number + " .description").val(localStorage.getItem(number));
+    }
+}
+
+saveTask();
